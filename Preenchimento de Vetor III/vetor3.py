c5846aa0aa3863f9148f9a1818c9b8bdf34fa89b
Ivanildo Silva <ijsfilho+github@gmail.com>
2021-10-06 14:59:27 -0300

    Create vetor3.py

---
+++ Preenchimento de Vetor III/vetor3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Preenchimento de Vetor III
+Implementa desafio de código 'Preenchimento de Vetor III' em Python.
+Para mais detalhes consulte o README.md.
+"""
+__version__ = "1.0"
+__author__  = "Ivanildo Silva"
+__email__   = "ijsfilho@gmail.com"
+
+n=[float(input())]
+
+for i in range(100):
+    print('N[{}] = {:.4f}'.format(i, n[i]))
+    if i != 99:
+        n.append(n[i]/2)
